aeff79377b101397b96796f38da6789f4b835eda
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-09-09 20:11:20 -0700

    import mutations signUp

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -45,10 +45,8 @@ const LoginForm = () => {
       console.error(error);
       setShowAlert(true);
     }
-    }
 
     setUserFormData({
-      username: '',
       email: '',
       password: '',
     });
