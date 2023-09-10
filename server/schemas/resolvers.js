cf239e3d66867944e010eb78ffa8ae0ecd825b4f
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-09-09 18:45:36 -0700

    const resolvers/query

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+              data = await User.findOne({ _id: context.user._id }).select('-__v -password');
+              return data;
+            }
+            throw new AuthenticationError('You must be logged in!');
+        },
+    },
+
+    
+};
+
+module.exports = resolvers;
